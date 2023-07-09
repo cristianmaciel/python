e59bd4e1ad2e14eac1180ba1e34473c752a7e91b
Cristian Maciel Pereira <107712125+cristianmaciel@users.noreply.github.com>
2023-07-09 19:49:18 -0300

    Update Manupulaçao_De_Arquivos.py

--- Manupulaçao_De_Arquivos.py
+++ Manupulaçao_De_Arquivos.py
@@ -50,4 +50,61 @@ def cadastrar(arq, dados1='desconhecido', dados2=0):
         else:
             print(f'\033[0;32mNovo registo de {dados1} adicionado com Sucesso\033[m')
             a.close()
+
+#Excluir dados cadastrado no arquivo
+def excluirDado(arq, dado_pesquisado):
+    try:
+        with open(arq, 'r') as arquivo:
+            linhas = arquivo.readlines()
+
+        encontrado = False
+
+        with open(arq, 'w') as arquivo:
+            for linha in linhas:
+                if dado_pesquisado not in linha:
+                    arquivo.write(linha)
+                else:
+                    encontrado = True
+
+        if encontrado:
+            print(f'\033[0;32mO dado "{dado_pesquisado}" foi excluído com sucesso.\033[m')
+        else:
+            print(f'\033[0;33mO dado "{dado_pesquisado}" não foi encontrado para exclusão.\033[m')
+
+    except FileNotFoundError:
+        print(f'\033[0;31mO arquivo "{arq}" não foi encontrado.\033[m')
+
+
+#linha do Menu
+def linha(tam=42):
+    return ('-'*tam)
+
+#Cabeçalho do sistema
+def cabecalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+#Menu do Sistema
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    c=1
+    for item in lista:
+        print(f'\033[0;33m{c}\033[m - \033[0;34m{item}\033[m')
+        c=c+1
+    print(linha())
+    opc=leiaInt('Escolha uma Opção: ')
+    return opc
+
+#Entrada só com valores inteiro
+def leiaInt(msg):
+    while True:
+        try:
+            m=int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO Digite uma valor Inteiro!\033[m')
+            continue
+        else:
+            return m
+    
     
