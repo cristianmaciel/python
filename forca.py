8b08c374f52706f909ca6989652e8cd3e4556a8b
Cristian Maciel Pereira <107712125+cristianmaciel@users.noreply.github.com>
2023-07-22 17:52:08 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,20 +1,59 @@
-import random
-palavra=['BANANA','LARANJA','MORANGO','JACA','UVA','PERA']
-chave=random.choice(palavra)
-while True:
-    nome=str(input('Digite a Letra de uma Fruta: ')).upper()[0]
-    if chave.upper()[0] == nome:
-        print('\033[0;32mAcertou Misseraval hehe\033[m')
-        print(f'\033[0;33mA Palavra Secreta é\033[m \033[0;32m{chave}\033[m')
-        break
-    else:
-        print(f'\033[0;31mErrou a palavra nao começa com {nome}\033[m')
+import os
+import time
+from random import randrange
+#Entrado de dados para aceitar somento numero inteiros
+def leiaInt(msg):
     while True:
-        status=str(input('Quer continuar S/N? ')).upper()[0]
-        if status in 'NS':
-            break
-        print('Digite S pra Sim ou N pra Não:')
-    if status == 'N':
-        break
-print('\033[0;33mEncerrando o Sistema.......\033[m')
+        try:
+            m=int(input(msg))
+        except:
+            print('\033[0;31mERRO Digite um numero Inteiro!\033[m')
+        else:
+            return m
 
+#menu princial do jogo       
+def menuForca(msg):
+    print('\033[0;34m*********************************\033[m')
+    print('\033[0;34m***{msg}***\033[m')
+    print('\033[0;34m*********************************\033[m')
+    
+#MAIN PROGRAMA PRINCIPAL 
+arquivo=open('palavras.txt', 'r')
+palavras = []
+for linha in arquivo:
+    linha = linha.strip()
+    palavras.append(linha)
+arquivo.close()
+numero=randrange(0,len(palavras))
+palavra_secreta=palavras[numero].upper()
+letras_acertada=['_'for letra in palavra_secreta]
+acertou=False
+enforcou=False
+erros=0
+os.system('clear')
+menuForca('Bem vindo ao jogo da Forca')
+print(f'\033[0;33m{letras_acertada}\033[m')
+while (not enforcou and not acertou):
+    chute=str(input('\033[0;32mQual a letra? \033[m')).upper()
+    chute = chute.strip().upper()
+    os.system('clear')
+    menuForca('Bem vindo ao jogo da Forca')
+    if chute in palavra_secreta:
+        posicao=0
+        for letra in palavra_secreta:
+            if chute == letra:
+                letras_acertada[posicao]=letra
+            posicao=posicao+1
+    else:
+        erros=erros+1
+    acertou='_'not in letras_acertada
+    enforcou=erros==6
+    print(f'\033[0;33m{letras_acertada}\033[m')
+if acertou:
+    print('\033[0;32mVocê Ganhou!\033[m')
+else:
+    print('\033[0;31mVocê Perdeu!\033[m')
+time.sleep(2)
+print('\033[0;31mFim do Jogo\033[m')
+time.sleep(2)
+os.system('clear')
